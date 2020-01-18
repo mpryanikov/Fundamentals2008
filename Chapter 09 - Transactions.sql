ee0066cb1185bb1825072ab689db6394b1f45c28
m.pryanikov <KTD_2007@mail.ru>
2020-01-18 21:35:07 +0300

    Isolation Levels The REPEATABLE READ Isolation Level COMMIT TRAN;

--- Chapter 09 - Transactions.sql
+++ Chapter 09 - Transactions.sql
@@ -58,13 +58,6 @@ DBCC CHECKIDENT ('Sales.Orders', RESEED, 11077);
 
 --////////////
 
--- Connection 1
-  SELECT productid, unitprice
-  FROM Production.Products
-  WHERE productid = 2;
-
-COMMIT TRAN;
-
 -- Cleanup
 UPDATE Production.Products
   SET unitprice = 19.00
--- Chapter 9 - Transactions/Connection 1.sql
+++ Chapter 9 - Transactions/Connection 1.sql
@@ -1,11 +1,9 @@
 -- Connection 1
-SET TRANSACTION ISOLATION LEVEL REPEATABLE READ;
-
-BEGIN TRAN;
-
   SELECT productid, unitprice
   FROM Production.Products
   WHERE productid = 2;
+
+COMMIT TRAN;
 
 /*
  productid     unitprice    
