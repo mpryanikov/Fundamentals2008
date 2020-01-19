da1262b1d81ea181c55eb5ded6c3fd93372e7f6b
m.pryanikov <KTD_2007@mail.ru>
2020-01-19 15:16:03 +0300

    Cleanup

--- Chapter 09 - Transactions.sql
+++ Chapter 09 - Transactions.sql
@@ -56,19 +56,3 @@ WHERE orderid > 11077;
 
 DBCC CHECKIDENT ('Sales.Orders', RESEED, 11077);
 
---////////////
-
--- Cleanup
-UPDATE Production.Products
-  SET unitprice = 19.00
-WHERE productid = 2;
-
-UPDATE Sales.OrderDetails
-  SET unitprice = 19.00
-WHERE productid = 2
-  AND orderid >= 10500;
-
-UPDATE Sales.OrderDetails
-  SET unitprice = 15.20
-WHERE productid = 2
-  AND orderid < 10500;
