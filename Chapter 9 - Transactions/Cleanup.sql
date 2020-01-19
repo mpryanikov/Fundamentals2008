f3de6dba5bc30ca968e812273e3e4d0e9da9af60
m.pryanikov <KTD_2007@mail.ru>
2020-01-19 14:24:28 +0300

    Cleanup

--- Chapter 9 - Transactions/Cleanup.sql
+++ Chapter 9 - Transactions/Cleanup.sql
@@ -1,8 +1,4 @@
 -- Cleanup
-DELETE FROM Production.Products
-WHERE productid > 77;
-
-DBCC CHECKIDENT ('Production.Products', RESEED, 77);
-
--- In all connections issue:
-SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+UPDATE Production.Products
+  SET unitprice = 19.00
+WHERE productid = 2;
