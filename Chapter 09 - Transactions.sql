21c2c9a9210b45ad828c7341c71dc9e39fc78621
m.pryanikov <KTD_2007@mail.ru>
2020-01-19 14:56:40 +0300

    Cleanup

--- Chapter 09 - Transactions.sql
+++ Chapter 09 - Transactions.sql
@@ -58,19 +58,6 @@ DBCC CHECKIDENT ('Sales.Orders', RESEED, 11077);
 
 --////////////
 
--- Cleanup
-UPDATE Production.Products
-  SET unitprice = 19.00
-WHERE productid = 2;
-
--- Close all connections
-
--- Make sure you're back in default mode
-SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
-
--- Change database options to default
-ALTER DATABASE TSQLFundamentals2008 SET ALLOW_SNAPSHOT_ISOLATION OFF;
-ALTER DATABASE TSQLFundamentals2008 SET READ_COMMITTED_SNAPSHOT OFF;
 
 ---------------------------------------------------------------------
 -- Deadlocks
--- Chapter 9 - Transactions/Cleanup.sql
+++ Chapter 9 - Transactions/Cleanup.sql
@@ -4,3 +4,10 @@ UPDATE Production.Products
 WHERE productid = 2;
 
 -- Close all connections
+
+-- Make sure you're back in default mode
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
+
+-- Change database options to default
+ALTER DATABASE TSQLFundamentals2008 SET ALLOW_SNAPSHOT_ISOLATION OFF;
+ALTER DATABASE TSQLFundamentals2008 SET READ_COMMITTED_SNAPSHOT OFF;
